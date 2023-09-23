157aa87a5bccb7f47ca480c5e5381fc47866bac7
adiee5 <58359866+adiee5@users.noreply.github.com>
2023-09-23 11:47:41 +0200

    Update compile.bat - fixed a wrong command within the script

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 if exist ".\SHELL-CMDS\" (
-if exist ".\SHELL-CMDS\neofetch" rem .\SHELL-CMDS\neofetch
+if exist ".\SHELL-CMDS\neofetch" del .\SHELL-CMDS\neofetch
 )
 else md SHELL-CMDS
 java -jar %PROG8C% neofetch.p8 -target cx16
